3b76a83be79e8ef0c78c2fe364daa943ee8f9f99
ik012982i9 <jurrutiabe22dw@ikzubirimanteo.com>
2023-03-21 10:28:46 +0100

    Initial commit

---
+++ src/ventana.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ventana {
+
+    public ventana() {
+        JFrame frame = new JFrame("XLR8 Transports");
+        frame.setSize(1280,720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout());
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+
+        
+
+
+
+
+        frame.setVisible(true);
+    }
+
+}
