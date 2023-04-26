f86e493876c13e9f865e52d9a552a890f25e7678
ik012982i9 <oslimani22dw@ikzubiri.manteo.com>
2023-04-26 09:02:30 +0200

    .

--- src/Windows/reparto.java
+++ src/Windows/reparto.java
@@ -1,7 +1,6 @@
 package Windows;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -42,59 +41,16 @@ public class reparto extends JFrame {
         //=====================
 
             JPanel mainPanel = new JPanel();
-            mainPanel.setLayout(new GridLayout(1, 2));
+            mainPanel.setLayout(new GridLayout(1, 3));
 
             // Primer ejemplo de reparto
-            JPanel prin=new JPanel();
-            prin.setLayout(new BoxLayout(prin,BoxLayout.Y_AXIS));
-            prin.setPreferredSize(new Dimension(100 , 100));
-            prin.setBorder(BorderFactory.createTitledBorder("Servicio 1"));
-
             JPanel panel1 = new JPanel();
-
-            //panel1.setLayout(new BoxLayout(panel1,BoxLayout.Y_AXIS));
+            panel1.setPreferredSize(new Dimension(100 , 100));
+            panel1.setBorder(BorderFactory.createTitledBorder("Servicio 1"));
+            panel1.setLayout(new FlowLayout());
             panel1.add(new JLabel("Internacional"));
             panel1.add(new JLabel(new ImageIcon("src\\Windows\\images\\internacional.jpeg")));
-            prin.add(panel1);
-            panel1.setPreferredSize(new Dimension(100, 50));
-
-
-        // Create the JPanel and set its layout manager
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(100, 50));
-
-// Create the icon and label
-        ImageIcon icon = new ImageIcon("src\\Windows\\images\\chimicicon.png");
-        Image scaledImage = icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        JLabel iconLabel = new JLabel(new ImageIcon(scaledImage));
-        iconLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-
-// Create the center label
-        JLabel centerLabel = new JLabel("Chemical Materials");
-        centerLabel.setFont(new Font("Arial", Font.BOLD, 16));
-
-// Create the price label
-        JLabel priceLabel = new JLabel("Starting from $1000");
-        priceLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-
-// Add the components to the panel
-        panel.add(iconLabel, BorderLayout.WEST);
-        panel.add(centerLabel, BorderLayout.CENTER);
-        panel.add(priceLabel, BorderLayout.EAST);
-
-// Add some padding and spacing between the components
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        panel.add(Box.createHorizontalStrut(10), BorderLayout.WEST);
-        panel.add(Box.createHorizontalStrut(10), BorderLayout.CENTER);
-        panel.add(Box.createHorizontalStrut(10), BorderLayout.EAST);
-        Border border = BorderFactory.createTitledBorder("Chemical Materials Service");
-
-// Set the border for the panel
-        panel.setBorder(border);
-        prin.add(panel);
-
-
-        mainPanel.add(prin);
+            mainPanel.add(panel1);
 
             // Segundo ejemplo de reparto
             JPanel panel2 = new JPanel();
